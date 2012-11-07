7fe436032cdb94b9b5bef6edea0d6daab8adddbe
kellydunn <defaultstring@gmail.com>
2012-11-06 21:27:55 -0800

    [src] Now doing async io

--- src/glenn.c
+++ src/glenn.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <libusb.h>
 
 //SIFTEO_VENDOR_ID=0x22fa
+
+void usb_callback(struct libusb_transfer * transfer) {
+  int i;
+  printf("%d bytes transferred: \n", transfer->length);
+  printf("result: %s\n",transfer->buffer);
+}
 
 void poll_usb_data(libusb_device * dev){
   libusb_device_handle *handle;
@@ -14,23 +21,30 @@ void poll_usb_data(libusb_device * dev){
   }
   libusb_claim_interface(handle,0);
 
+  struct libusb_transfer * transfer;
+  transfer = libusb_alloc_transfer(0);
+
+
   char data[8];
   int bytes_transferred;
-  libusb_bulk_transfer(handle, 
-                       0x81,
-                       data,
-                       sizeof(data),
-                       &bytes_transferred,
-                       1000);
-  
-  libusb_release_interface(handle, 0);
+  libusb_fill_bulk_transfer(transfer,
+                            handle, 
+                            0x81,
+                            data,
+                            sizeof(data),
+                            usb_callback,
+                            &bytes_transferred,
+                            1000);
+
+  libusb_submit_transfer(transfer);
+  //libusb_free_transfer(transfer);
+  //libusb_release_interface(handle, 0);
   
   if(kernel == 1) {
     libusb_attach_kernel_driver(handle, 0);
   }
 
-  printf("%d bytes transferred: %s\n", bytes_transferred, data);
-  libusb_close(handle);
+  //libusb_close(handle);
 }
 
 int main (){
@@ -53,6 +67,12 @@ int main (){
       printf("Sifteo Device #%02x detected.\n", desc.idProduct);
       poll_usb_data(dev);
     }
+  }
+
+  int err;
+  for(;;){
+    err = libusb_handle_events_completed(ctx, NULL);
+    printf("ERROR: %s\n", libusb_error_name(err));
   }
 
   libusb_free_device_list(devs, 1);
